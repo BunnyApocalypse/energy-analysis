40bd8a40c803299d394c981faeb836d41e9bd0b8
BunnyApocalypse <bunnyapocalypse@protonmail.com>
2018-12-01 15:14:26 -0600

    Added function to create ratio table of usage

--- data-wranglin.rkt
+++ data-wranglin.rkt
@@ -58,31 +58,73 @@
 ;;; Postconditions:
 ;;;
 (define clean-reformatted-energy-data
- (lambda (table)
-   (let kernel ([clean-tbl null]
-                [tbl-remaining (reverse (cdr table))]
-                [clean-lst null]
-                [lst-remaining (reverse (car table))])
-     (cond  [(null? tbl-remaining)
-             (cons (car table) clean-tbl)]
-            [(null? lst-remaining)
-             (kernel (cons clean-lst clean-tbl)
-                     (cdr tbl-remaining)
-                     null
-                     (reverse (car tbl-remaining)))]
-            [(string->number (car lst-remaining))
-             (kernel clean-tbl
-                     tbl-remaining
-                     (cons (car lst-remaining) clean-lst)
-                     (cdr lst-remaining))]
-            [else
-             (kernel clean-tbl
-                     (cdr tbl-remaining)
-                     null
-                     (reverse (car tbl-remaining)))]))))
+  (lambda (table)
+    (let kernel ([clean-tbl null]
+                 [tbl-remaining (reverse (cdr table))]
+                 [clean-lst null]
+                 [lst-remaining (reverse (car table))])
+      (cond  [(null? tbl-remaining)
+              (cons (car table) clean-tbl)]
+             [(null? lst-remaining)
+              (kernel (cons clean-lst clean-tbl)
+                      (cdr tbl-remaining)
+                      null
+                      (reverse (car tbl-remaining)))]
+             [(string->number (car lst-remaining))
+              (kernel clean-tbl
+                      tbl-remaining
+                      (cons (car lst-remaining) clean-lst)
+                      (cdr lst-remaining))]
+             [else
+              (kernel clean-tbl
+                      (cdr tbl-remaining)
+                      null
+                      (reverse (car tbl-remaining)))]))))
+;FIXME wasted work in working-row definitions
+(define create-ratio-table
+  (lambda (usage-table production-table)
+    (let kernel ([new-table null]
+                 [tbl1 (reverse (cdr usage-table))]
+                 [tbl2 (reverse (cdr production-table))]
+                 [working-row-1 (car (reverse (cdr usage-table)))]
+                 [working-row-2 (car (reverse (cdr production-table)))]
+                 [counter 0]
+                 [row null])
+      (cond
+        [(or (= 1 (length tbl1)) (= 1 (length tbl2)))
+         new-table]
+        [(= counter 13)
+         (kernel
+          (cons (reverse row) new-table)
+          (cdr tbl1)
+          (cdr tbl2)
+          (car (cdr tbl1))
+          (car (cdr tbl2))
+          0
+          null)]
+        [(= counter 0)
+         (kernel
+          new-table
+          tbl1
+          tbl2
+          (cddr working-row-1)
+          (cddr working-row-2)
+          (+ counter 1)
+          (cons (cadr working-row-1) (cons (car working-row-1) row)))]
+        [else
+         (kernel
+          new-table
+          tbl1
+          tbl2
+          (cdr working-row-1)
+          (cdr working-row-2)
+          (+ counter 1)
+          (cons (if (equal? "0" (car working-row-2))
+                    "Production was 0"
+                    (/ (string->number (car working-row-1)) (string->number (car working-row-2)))) row))]))))
 
 (define reformatted-production-data
-  (clean reformatted-energy-data (reformat-energy-data energy-production-data)))
+  (clean-reformatted-energy-data (reformat-energy-data energy-production-data)))
 (define reformatted-consumption-data
   (clean-reformatted-energy-data (reformat-energy-data energy-consumption-data)))
-
+(define test (create-ratio-table reformatted-consumption-data reformatted-production-data))
